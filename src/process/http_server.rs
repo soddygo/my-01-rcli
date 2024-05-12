3ddba292e6d70bc522ea523ad58a3eadbd916ff3
soddy <soddygo@qq.com>
2024-05-13 00:07:42 +0800

    temp save

--- src/process/http_server.rs
+++ src/process/http_server.rs
@@ -1,33 +1,50 @@
+use std::convert::Infallible;
+use std::io::Bytes;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::Duration;
 
 use anyhow::Result;
 use axum::extract::{Path, State};
-use axum::http::StatusCode;
+use axum::http::{HeaderName, Request, Response, StatusCode};
 use axum::Router;
 use axum::routing::get;
+use tower::{Service, ServiceBuilder, ServiceExt};
+use tower_http::body::Full;
+use tower_http::ServiceBuilderExt;
 use tracing::{info, warn};
 use tower_http::services::ServeDir;
 
 #[derive(Debug)]
-struct HttpServeState{
-    path:PathBuf
+struct HttpServeState {
+    path: PathBuf,
 }
 
 
-
-pub async fn process_http_server(path:PathBuf,port:u16)->Result<()>{
+pub async fn process_http_server(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving {:?} on {}", path, addr);
 
     let state = HttpServeState { path: path.clone() };
     // axum router
+    // let mut service = ServiceBuilder::new()
+    //     // Methods from tower
+    //     .timeout(Duration::from_secs(30))
+    //     // Methods from tower-http
+    //     .trace_for_http()
+    //     .propagate_header(HeaderName::from_static("x-request-id"))
+    //     .service_fn(handle); 
+
     let router = Router::new()
         .nest_service("/tower", ServeDir::new(path))
+
         .route("/*path", get(file_handler))
         .with_state(Arc::new(state));
-
+    // // 将服务和路由器组合在一起
+    // let app = router.into_service() // 转换为服务
+    //     .and_then(move |req, res| service.call(req).map(move |res| (res, res)));
+    // 
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
     Ok(())
@@ -50,14 +67,31 @@ async fn file_handler(
         // if it is a directory, list all files/subdirectories
         // as <li><a href="/path/to/file">file name</a></li>
         // <html><body><ul>...</ul></body></html>
-        match tokio::fs::read_to_string(p).await {
-            Ok(content) => {
-                info!("Read {} bytes", content.len());
-                (StatusCode::OK, content)
+
+        if p.is_dir() {
+            let mut content = "<html><body><ul>".to_string();
+            let mut read_dir = tokio::fs::read_dir(p).await.expect("Failed to read directory");
+
+            while let Some(entry) = read_dir.next_entry().await.expect("Failed to get next entry") {
+                let path = entry.path();
+                if let Some(name) = path.file_name() {
+                    let name = name.to_string_lossy();
+                    content += &format!("<li><a href=\"{}\">{}</a></li>", name, name);
+                }
             }
-            Err(e) => {
-                warn!("Error reading file: {:?}", e);
-                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
+
+            content += "</ul></body></html>";
+            (StatusCode::OK, content)
+        } else {
+            match tokio::fs::read_to_string(p).await {
+                Ok(content) => {
+                    info!("Read {} bytes", content.len());
+                    (StatusCode::OK, content)
+                }
+                Err(e) => {
+                    warn!("Error reading file: {:?}", e);
+                    (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
+                }
             }
         }
     }
